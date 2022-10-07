fd9dc482d7471086f812508a03332522172ae7eb
Bennett Goble <signal@lindenlab.com>
2022-10-07 14:15:24 -0700

    Build windows with msbuild

--- build-cmd.sh
+++ build-cmd.sh
@@ -44,38 +44,14 @@ case "$AUTOBUILD_PLATFORM" in
 
     load_vsvars
 
-    # We've observed some weird failures in which the PATH is too big to be
-    # passed to a child process! When that gets munged, we start seeing errors
-    # like failing to understand the 'nmake' command. Thing is, by this point
-    # in the script we've acquired a shocking number of duplicate entries.
-    # Dedup the PATH using Python's OrderedDict, which preserves the order in
-    # which you insert keys.
-    # We find that some of the Visual Studio PATH entries appear both with and
-    # without a trailing slash, which is pointless. Strip those off and dedup
-    # what's left.
-    # Pass the existing PATH as an explicit argument rather than reading it
-    # from the environment to bypass the fact that cygwin implicitly converts
-    # PATH to Windows form when running a native executable. Since we're
-    # setting bash's PATH, leave everything in cygwin form. That means
-    # splitting and rejoining on ':' rather than on os.pathsep, which on
-    # Windows is ';'.
-    # Use python -u, else the resulting PATH will end with a spurious '\r'.
-    export PATH="$(python -u -c "import sys
-from collections import OrderedDict
-print(':'.join(OrderedDict((dir.rstrip('/'), 1) for dir in sys.argv[1].split(':'))))" "$PATH")"
-
-    export PATH="$(python -u -c "import sys
-print(':'.join(d for d in sys.argv[1].split(':')
-if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', 'Team Tools'))))" "$PATH")"
-
-    python -c "print(' PATH '.center(72, '='))"
-    cygpath -p -m "$PATH" | tr ';' '\n'
-    python -c "print(' ${#PATH} chars in PATH '.center(72, '='))"
-
     which nmake
 
-    for proj in apr aprutil apriconv xml libapr  libaprutil libapriconv
-      do build_sln "apr-util/aprutil.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM" "$proj"
+    for proj in apr aprutil apriconv xml libapr libaprutil libapriconv
+      msbuild.exe \
+        -t:$proj \
+        -p:Configuration=Release \
+        -p:Platform=$AUTOBUILD_WIN_VSPLATFORM \
+        -p:PlatformToolset=v143
     done
 
     mkdir -p "$RELEASE_OUT_DIR" || echo "$RELEASE_OUT_DIR exists"
